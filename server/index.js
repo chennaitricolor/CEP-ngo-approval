dab343ec369a708c110fde631597e0b2fbd382fb
Suryaprakash <suryaprakash.m@thoughtworks.com>
2019-10-27 10:46:32 +0530

    Added Cron to remove temp files every 10 mins

--- server/index.js
+++ server/index.js
@@ -5,30 +5,30 @@ const azureStorage = require('azure-storage');
 const orgainsation = require('./hasuraClient');
 
 const app = express();
+const PORT = 4000;
+const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+const AZURE_STORAGE_CONTAINER = process.env.AZURE_STORAGE_CONTAINER;
+const azureStorageConfig = {
+    accountName: STORAGE_ACCOUNT_NAME,
+    accountKey: ACCOUNT_ACCESS_KEY,
+}; 
+
+const TEMP_DIR = path.join(__dirname,'/../temp');
+app.use('/documents',express.static(TEMP_DIR));
+app.use('/organisation', orgainsation);
 app.use(cors());
 
-const tempDir = path.join(__dirname,'/../temp');
-app.use('/documents',express.static(tempDir));
-app.use('/organisation', orgainsation);
-
-const port = 4000;
 
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
-const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
-
-const azureStorageConfig = {
-    accountName: STORAGE_ACCOUNT_NAME,
-    accountKey: ACCOUNT_ACCESS_KEY,
-}; 
 function getFile(blobName){
-    const downloadFilePath = path.join(tempDir,blobName);
+    const downloadFilePath = path.join(TEMP_DIR,blobName);
     return new Promise((resolve, reject) => {
         const blobService = azureStorage.createBlobService(azureStorageConfig.accountName, azureStorageConfig.accountKey); 
-        blobService.getBlobToLocalFile("demo", blobName, downloadFilePath, function(error, serverBlob) {
+        blobService.getBlobToLocalFile(AZURE_STORAGE_CONTAINER, blobName, downloadFilePath, function(error, serverBlob) {
             if (error) {
                 console.log("^^^^",error)
                 resolve(error);
@@ -44,6 +44,6 @@ app.get('/downloadFile/:file',async (req,res)=> {
     res.send(fileName);
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`));
